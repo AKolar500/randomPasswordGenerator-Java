ed90f2765651afce836578559032c2f89593914f
AKolar500 <76533984+AKolar500@users.noreply.github.com>
2021-04-15 11:40:13 -0700

    firstCommit

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * This project will generate a random password of 8-10 digits, 
+ * with at least one capital, one lowercase, one number, and one symbol
+ */
+/**
+ * @author alexk
+ *
+ */
+module randomPasswordGeneratorReadme {
+}
---
+++ src/passwordGenerator/MainClass.java
@@ -0,0 +1,40 @@
+package passwordGenerator;
+
+import java.util.Random;
+
+public class MainClass {
+
+	//I want a password with a length of 8
+	static final int output_password_length = 8;
+	
+	public static void main(String[] args) {
+		//these are the characters from which the generator chooses
+		String strPossibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+		//initialize Random
+		Random random = new Random();
+		
+		//create 10 passwords, just in case I don't like the first one
+		System.out.println("Choose a password from the options below.");
+		for(int i=0; i<10; i++) {
+			System.out.println( getNextRandomPassword(strPossibleChars, random) );
+		}
+	}
+
+	//create the passwords
+	private static String getNextRandomPassword(String strPossibleChars, Random random) {
+		StringBuilder sbRandomString = new StringBuilder(output_password_length);
+		
+		for(int i = 0 ; i < output_password_length; i++){
+            
+            //get next random character in the password
+            int randomInt = random.nextInt(strPossibleChars.length());
+            
+            //add each random character to the password string
+            sbRandomString.append( strPossibleChars.charAt(randomInt) );
+        }
+        
+        return sbRandomString.toString();
+	}
+
+}
